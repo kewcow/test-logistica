dc9568923deeaf1c9f038609492511e49f5c226f
kewcow <kevjhoss@gmail.com>
2022-11-09 15:09:27 -0300

    version 3.5

--- core/sessions.php
+++ core/sessions.php
@@ -1,7 +1,7 @@
 <?php
 //Creamos una funcion para cuando el usuario no este autenticado, hace el redirect al login
 function usuario_autenticado() {
-    if(true) {
+    if(revisar_usuario()) {
         $host = $_SERVER['HTTP_HOST'];
         $ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
         $html = 'index.php?controller=Login&action=index';
@@ -12,7 +12,7 @@ function usuario_autenticado() {
 }
 // Esta funcion revisa que exista la variable de session email, si existe quiere decir que el usuario se autentico con exito
 function revisar_usuario() {
-    return !isset($_SESSION['email']) && $_GET['controller'] != 'Login';
+    return !isset($_SESSION['email']); //&& $_GET['controller'] != 'Login';
 }
 //ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/session'));
 session_start();
